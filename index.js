55f5eee7099c4ba0d2ffad01def4542d5c2ef392
lufeng <lufeng@bingosoft.net>
2019-05-08 16:58:41 +0800

    inital commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const TemplateRelease = require('./template-release');
+
+module.exports = {
+    TemplateRelease,
+    constants: {
+        templateReleaseUrl: "https://api.github.com/repos/bingo-oss/bui-weex-template/releases",
+        cacheDirName: 'bui-weex'
+    }
+}
